d2d1d33ff48141bdbdc65d81444f53ca8d577c27
Aleksey Karelin <akarelins125@gmail.com>
2024-03-02 18:00:11 +0200

    Not Found finished, transitions added to navbar

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -7,7 +7,7 @@ const NotFound = () => {
         <div id='not-found'>
             <h1>404</h1>
 
-            <h3>I think, you are not supposed to be here.</h3>
+            <h3>I don't think that you are not supposed to be here.</h3>
         </div>
     );
 }
